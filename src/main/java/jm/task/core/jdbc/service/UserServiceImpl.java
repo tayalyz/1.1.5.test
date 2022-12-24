edf13ec6f966a9fedbb24169bf5485bb7d03640d
tayalyz <107634713+tayalyz@users.noreply.github.com>
2022-12-24 04:12:09 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private static UserDao ud = new UserDaoHibernateImpl();
+    private UserDao ud = new UserDaoHibernateImpl();
     public void createUsersTable() {
         ud.createUsersTable();
     }
