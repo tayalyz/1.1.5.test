ee60baf160194ad76cd3d3593f7fbcd22ff2750b
tayalyz <107634713+tayalyz@users.noreply.github.com>
2022-12-24 04:03:06 +0300

    Update User.java

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,21 +1,23 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity
+@Table(name = "userS")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    @Basic(optional = false)
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "lastName")
     private String lastName;
 
-    @Column
+    @Column(name = "age")
     private Byte age;
 
     public User() {
@@ -59,7 +61,7 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
-    
+
     @Override
     public String toString() {
         return "User{" +
